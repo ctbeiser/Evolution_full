5437600b996acc7d0090947db56fabb75fc410c9
Chris Beiser <ctbeiser@gmail.com>
2016-04-24 18:14:53 -0400

    Fixes for documentation

--- 13/evolution/server.py
+++ 13/evolution/server.py
@@ -15,29 +15,26 @@ class Server:
         :param port: an Integer representing a port number.
         Note: If the port is already in use, this function will exit(1)
         """
-        self.host = host
-        self.port = port
-
-        self.sock = self.initialize_socket()
+        # The listener socket for this server— a socket.Socket object
+        self.sock = self.initialize_socket(host, port)
+        # A list of (JSONSocket, String) representing the socket for a player and their handshake message
         self.connected_players = []
 
     def add_players(self):
-        """ Connect players to the Server until 8 have connected or the timer runs out
-        :return: A list of players
+        """ Connect players to the Server until at least the minimum have connected, and up to the maximum have
+        :return: A list of (JSONSockets mapping to each player.
         """
         while len(self.connected_players) < MIN_PLAYERS:
             try:
                 self.check_for_new_players()
             except TimedOutError:
                 pass
-        self.add_to_8()
+        self.add_to_max()
         return self.connected_players
 
     @timeout(5)
-    def add_to_8(self):
-        """
-        Add players to the server until 8 have connected or the timer runs out
-        :return:
+    def add_to_max(self):
+        """ Add more players to the server until 8 have connected or the timer runs out. It
         """
         while len(self.connected_players) < MAX_PLAYERS:
             try:
@@ -46,6 +43,9 @@ class Server:
                 break
 
     def check_for_new_players(self):
+        """ Check whether there are new players waiting to connect,
+        adds players the the list of connected players.
+        """
         x = self.blocking_call_to_socket_to_get_players()
         if x:
             clientsocket, addr = x
@@ -53,25 +53,37 @@ class Server:
 
     @timeout(5)
     def blocking_call_to_socket_to_get_players(self):
+        """ Calls accept on the socket to get new players. It's necessary to break this out to make it impossible for
+        the timeout to interrupt after we've had the handshake with the player.
+        :return: a Socket.socket client socket.
+        Note: this will throw a TimedOutError if there's no player to get
+        """
         return self.sock.accept()
 
     def add_new_player(self, sock):
+        """ Create a JSONSocket for this socket, conduct a handshake, and add it to the list of players for this player
+        :param sock: a Socket.socket client socket
+        """
         player = JSONSocket(sock)
         info = player.decode()
         if is_string(info):
             player.encode("ok")
-            self.connected_players.append(player)
+            self.connected_players.append(player, info)
         else:
             player.shutdown()
 
-    def initialize_socket(self):
+    def initialize_socket(self, host, port):
         """ Create a socket for listening for requests
-        :return: a Socket.socket object, bound to the host and port of this server.
+        :param host: a String representing a hostname
+        :param port: an Integer representing the port to listen on
+        :return: a Socket.socket server socket
         Note: This function will exit(1) if the socket is in use.
         """
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
-            sock.bind((self.host, self.port))
+            # It is necessary to pass this function a Tuple of (Host, Port) because the API authors thought that
+            # it was more important to mirror over how this was done in C than make something sane
+            sock.bind((host, port))
         except ConnectionResetError:
             sock.close()
             debug("Server: Socket is already in use")
